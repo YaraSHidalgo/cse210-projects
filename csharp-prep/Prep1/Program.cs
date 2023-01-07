343fdaf25e6f8d9608a81aa9153f49d013b94347
YaraSHidalgo <u382878978@byui.edu>
2023-01-06 23:29:35 -0300

    Add comments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,12 +6,15 @@ class Program
     {
         Console.WriteLine("Welcome");
     
+        // ask the user first name and store on the string name.
         Console.Write("What is your first name? ");
         string name = Console.ReadLine();
 
+        // ask the user last name and store on the string lastName.
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
+        // Print all the data name and lastName string.
         Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
 
     }
