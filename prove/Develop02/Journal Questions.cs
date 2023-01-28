1df2aefb497b8bf7a94267add9bffe409ea227be
YaraSHidalgo <u382878978@byui.edu>
2023-01-27 23:32:13 -0300

    Journal project changed

---
+++ prove/Develop02/Journal Questions.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Journal
+{
+    public List<string> questions = new List<string>()
+    {
+        "What was the best part of my day?",
+        "If I could have done something today, what would it be?",
+        "What was the most memorable feeling of my day today?",
+        "Which things am I thankful for today?",
+        "What is one thing I do not want to forget about this day?",
+    };
+
+    public void JournalQuestions()
+    {
+        foreach(string question in questions)
+        {
+            string oneQuestion = Console.ReadLine(questions);
+            Console.WriteLine($"{oneQuestion}");
+        }
+    }
+
+    public void JournalDateTime()
+    {
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();    
+        Console.WriteLine($"{dateText}");   
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+
+        Console.WriteLine("Welcome to the Journal Program! ");
+        Console.WriteLine();
+        
+        Menu menu = new Menu();
+        menu.MenuDisplay();
+
+        Journal journal = new Journal();
+        journal.JournalDateTime();
+
     }
 }
